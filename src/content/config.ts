a90b781a2498030f0c42b94e23a7dcba79781cc6
Yhonglan <57242276+Yhonglan@users.noreply.github.com>
2024-04-02 23:36:07 +0800

    change files

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const blog = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		tags: z.string().optional(),
+		tags: z.array(z.string()).optional(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						'code': {
+							'background-color': '#e5e9f0',
+							'border-radius': '3px',
+							'padding-left': '2px',
+							'padding-right': '2px'
+						},
+						'code::before': {
+							content: '""',
+						},
+						'code::after': {
+							content: '""',
+						}
+					},
+				},
+			},
+		},
 	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [
+		require('@tailwindcss/typography')
+	],
 }
